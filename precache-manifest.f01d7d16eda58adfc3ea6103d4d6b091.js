49d1b5ab8e829f8ed542a13a5307e73514e4c215
zdennyp <zdenekpetera77@gmail.com>
2021-02-22 00:15:05 +0100

    Updates

---
+++ precache-manifest.f01d7d16eda58adfc3ea6103d4d6b091.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78051d320b73cad1185437f194139a8",
+    "url": "/zdennyp/robofriends2/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/zdennyp/robofriends2/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e479b21cdb80f418f37b",
+    "url": "/zdennyp/robofriends2/static/css/main.bed849bf.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/zdennyp/robofriends2/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zdennyp/robofriends2/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e479b21cdb80f418f37b",
+    "url": "/zdennyp/robofriends2/static/js/main.947cafa5.chunk.js"
+  },
+  {
+    "revision": "7cee96f871f45f76fc3a",
+    "url": "/zdennyp/robofriends2/static/js/runtime-main.3866e5b9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/zdennyp/robofriends2/static/media/SEGA.12a3db8b.woff"
+  }
+]);
